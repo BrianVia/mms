45960f2a63ffa58b1c03667e2c20e17feddfff20
Brian Via <brian.via.dev@gmail.coma>
2019-02-20 17:36:00 -0500

    A few column selection changes
    Make Collection use ColumnSelection fragment, not sure why that wasn't the case>
    Add list icon for columns, but it's displaying weird.

--- webui/src/Fragments/ColumnSelection.js
+++ webui/src/Fragments/ColumnSelection.js
@@ -4,12 +4,13 @@ import { withStyles, withTheme } from '@material-ui/core/styles';
 
 
 import Input from '@material-ui/core/Input';
-import InputLabel from '@material-ui/core/InputLabel';
+// import InputLabel from '@material-ui/core/InputLabel';
 import MenuItem from '@material-ui/core/MenuItem';
 import FormControl from '@material-ui/core/FormControl';
 import ListItemText from '@material-ui/core/ListItemText';
 import Checkbox from '@material-ui/core/Checkbox';
 import Select from '@material-ui/core/Select';
+import List from '@material-ui/icons/List';
 
 const styles = theme => ({
     width: '50px'
@@ -22,29 +23,32 @@ class ColumnSelection extends Component {
     }
 
     render() {
-        const { classes } = this.props;
 
         return (
             <div>
-                <FormControl className={classes.width}>
-                    <InputLabel htmlFor="select-multiple-checkbox">Fields</InputLabel>
+                <FormControl>
                     <Select
+                        IconComponent={() => (
+                            <List />
+                          )}>
                         value={this.state.displayedColumns}
                         multiple
-                        onChange={this.props.onChange}
+                        onChange={this.onColumnSelectionChange}
                         input={<Input id="select-multiple-checkbox" />}
-                        renderValue={selected => selected.join(', ')}
+                        renderValue={selected => selected.join(", ")}
                     >
-                        {this.props.columns.map(column => (
-                            <MenuItem key={column.name} value={column.display}>
-                                <Checkbox checked={column.display} />
-                                <ListItemText primary={column.label} />
+                        {Object.keys(this.props.columns).map(key => (
+                            <MenuItem
+                                key={this.props.columns[key].name}
+                                value={this.props.columns[key].display}
+                            >
+                                <Checkbox checked={this.props.columns[key].display} />
+                                <ListItemText primary={this.props.columns[key].label} />
                             </MenuItem>
                         ))}
                     </Select>
                 </FormControl>
-
-            </div >
+            </div>
         )
     }
 
